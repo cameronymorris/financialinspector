72064aa0765c53e0cc6216897be34851da332be2
cameronymorris <adrianfdicus@gmail.com>
2018-06-19 23:47:12 +0300

    several issues fixed

---
+++ src/app/expense-list/financial-list/financial-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-financial-list',
+  templateUrl: './financial-list.component.html',
+  styleUrls: ['./financial-list.component.less']
+})
+export class FinancialListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  deleteItem(list, index, name){
+
+  }
+
+}
